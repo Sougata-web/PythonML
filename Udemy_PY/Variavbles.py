8d9a452eda7870f20edc64b2a07a027ed39a368f
Sougata Pal <palsougata256@gmail.com>
2025-06-06 17:02:34 +0530

    new file:   Training_Materials.zip 	new file:   Udemy_PY/Variavbles.py 	new file:   Udemy_PY/tempCodeRunnerFile.py

---
+++ Udemy_PY/Variavbles.py
@@ -0,0 +1,50 @@
+print(2)
+print("Hello")
+
+print(type("Hello"))
+print("Sougata "*5)
+
+
+message= "We are angineers"
+n=17
+pi=3.141592
+
+print(type(message),type(n),type(pi))
+#Undefined variable or ubound variable
+x=2
+print (x)
+# del x
+# print(x)
+
+# Identifiers
+x=2
+x2=2
+total=x+x2
+
+# Invalid Identifiers
+# sub-total = 44
+
+# You can't take 35 reserved keywords in python as an identifires
+
+
+# Operators and operands
+# +,-,*,/,**,//,%
+c= 60//2
+d=(0.3)**11
+print(c,d)
+
+# Pyhton follow PEMDAS rule for mathematical expressions
+# Parenthese, Exponential, Multiplication, Division, Addition, Substraction
+print(3*1**3)
+
+
+# How to ask data from the user?
+x=input("Enter the value of x: ")
+y=input("Enter the value of y: ")
+
+x=int(x)
+y=int(y)
+
+print(x+y)
+y=int(input("Enter the integer value: "))
+print(y)
