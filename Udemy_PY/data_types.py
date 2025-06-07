78d6aa2c076b3b2b563b90141c6353cce29677f6
Sougata Pal <palsougata256@gmail.com>
2025-06-07 16:22:14 +0530

    modified:   Udemy_PY/data_types.py

--- Udemy_PY/data_types.py
+++ Udemy_PY/data_types.py
@@ -16,3 +16,15 @@ print(Set)
 
 Dictionary={'a':'A', 2:'AA',True:1, False:0}
 print(Dictionary)
+
+
+x=1
+if x==1:
+ print("x == 1")
+ print("x is a bad bitch")
+elif x==2:
+    print("x == 2")
+else:
+    x==0
+    print("x == 0")
+    
