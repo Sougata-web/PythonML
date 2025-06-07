cb8d067047598ea811c030e1db6befe286421345
Sougata Pal <palsougata256@gmail.com>
2025-06-07 15:56:45 +0530

    renamed:    Udemy_PY/Variavbles.py -> Udemy_PY/Variables.py 	new file:   Udemy_PY/data_types.py

---
+++ Udemy_PY/data_types.py
@@ -0,0 +1,18 @@
+float_inf=float('inf')
+print(type(float_inf))
+
+
+complex_num=2+3j
+print(type(complex_num))
+
+List=[1,True,"ML"]  # Values can be changed
+print(type(List))
+
+Tuple=(1,True,"ML")  # Values cannot be changed
+print(type(Tuple))
+
+Set=set([1,3,2,2,4,5,6,4])  # There will not be any duplicates
+print(Set)
+
+Dictionary={'a':'A', 2:'AA',True:1, False:0}
+print(Dictionary)
